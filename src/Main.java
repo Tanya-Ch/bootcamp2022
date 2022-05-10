bb665b4adc7c1eafd5cded0deb9c9b6d67a0ebef
tania <tchermnykh@sumix.com>
2022-05-10 20:18:13 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+public class Main {
+    static int size = 8;
+    static int[][] desk = new int[size][size];
+    int[][] step = {{1,-2}, {2,-1}, {2,1}, {1,2}, {-1,2}, {-2,1}, {-2, -1}, {-1,-2}};
+    static int n;
+
+    public static void main(String [] args){
+        Main main = new Main();
+        n = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                desk[i][j] = 0;
+            }
+        }
+        main.setHorse(1,1);
+        main.print();
+    }
+
+    private void print(){
+        System.out.printf("n=%d\n", n);
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.printf(" %2d", desk[i][j]);
+            }
+            System.out.print("\n");
+        }
+    }
+    private boolean setHorse(int x, int y){
+        if((x < 0 || x >= size) || (y < 0 || y >= size)){
+            return false;
+        }
+        if(desk[x][y] != 0){
+            return false;
+        }
+        n++;
+        desk[x][y] = n;
+        if(n == size*size){
+            return true;
+        }
+        print();
+        for (int i = 0; i < 8; i++) {
+            if(setHorse(x + step[i][0], y + step[i][1])){
+                return true;
+            }
+        }
+        n--;
+        desk[x][y] = 0;
+        return false;
+    }
+}
